65552e8e38077b3c271f86b3abea30575f095e40
punkushal <spycymagar@gmail.com>
2024-12-27 16:22:51 +0545

    bottom navigation intial code added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,8 +9,40 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 1,
+        backgroundColor: Colors.blue.shade300,
+        title: const Text("Books"),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        currentIndex: currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.bookmark,
+            ),
+            label: 'Bookmarks',
+          ),
+        ],
+        selectedItemColor: Colors.blue.shade400,
+        unselectedItemColor: Colors.grey,
+      ),
+    );
   }
 }
